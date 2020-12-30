efc0180e22620e52fcf6c6d97780540d1f2af107
t4n-du4ng <dungnguyenq1w2@gmail.com>
2020-12-30 21:50:53 +0700

    Updates

---
+++ precache-manifest.d1aef59e6a60308d32a35786f2014156.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "341182f21642d8c9be5fe9aa4604baae",
+    "url": "/react__facebook-clone/index.html"
+  },
+  {
+    "revision": "971274d86d50b518a44f",
+    "url": "/react__facebook-clone/static/css/main.c5f7b973.chunk.css"
+  },
+  {
+    "revision": "6a9f7473d5fe6c83ccdb",
+    "url": "/react__facebook-clone/static/js/2.67444283.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react__facebook-clone/static/js/2.67444283.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "971274d86d50b518a44f",
+    "url": "/react__facebook-clone/static/js/main.9df59568.chunk.js"
+  },
+  {
+    "revision": "0c5b6a709ea5f463190b",
+    "url": "/react__facebook-clone/static/js/runtime-main.6f339902.js"
+  }
+]);
